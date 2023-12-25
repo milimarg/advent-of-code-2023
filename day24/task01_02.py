b005f08ce38115f266b36ae24526531f095ca86e
Florent GUITTRE <florent.guittre@epitech.eu>
2023-12-25 19:03:57 +0100

    feat: refactor variables of day24

--- day24/task01_02.py
+++ day24/task01_02.py
@@ -27,32 +27,29 @@ for line in lines:
 for part in [1, 2]:
     result = 0
     for j in range(len(positions)):
-        position2 = positions[j]
-        velocity2 = velocities[j]
+        px2, py2, pz2 = positions[j]
+        vx2, vy2, vz2 = velocities[j]
         if part == 1:
             for i in range(j):
-                position = positions[i]
-                velocity = velocities[i]
-                if velocity[1] * velocity2[0] == velocity[0] * velocity2[1]:
+                px, py, pz = positions[i]
+                vx, vy, vz = velocities[i]
+                if vy * vx2 == vx * vy2:
                     continue
-                x = ((position2[1] - position[1]) + position[0] * (velocity[1] / velocity[0]) - position2[0] * (
-                            velocity2[1] / velocity2[0])) / (
-                                (velocity[1] / velocity[0]) - (velocity2[1] / velocity2[0]))
-                t0 = (x - position[0]) / velocity[0]
+                x = (((py2 - py) + px * (vy / vx) - px2 * (vy2 / vx2)) /
+                     ((vy / vx) - (vy2 / vx2)))
+                t0 = (x - px) / vx
                 if t0 < 0:
                     continue
-                t1 = (x - position2[0]) / velocity2[0]
+                t1 = (x - px2) / vx2
                 if t1 < 0:
                     continue
-                y = position[1] + t0 * velocity[1]
+                y = py + vy * t0
                 result += (min_x <= x <= max_y) and (min_x <= y <= max_y)
         else:
-            px, py, pz = positions[j]
-            vx, vy, vz = velocities[j]
             time_i_second = z3.Real(f"t_{j}")
-            solver.add(px + vx * time_i_second == x_real + vx_real * time_i_second)
-            solver.add(py + vy * time_i_second == y_real + vy_real * time_i_second)
-            solver.add(pz + vz * time_i_second == z_real + vz_real * time_i_second)
+            solver.add(px2 + vx2 * time_i_second == x_real + vx_real * time_i_second)
+            solver.add(py2 + vy2 * time_i_second == y_real + vy_real * time_i_second)
+            solver.add(pz2 + vz2 * time_i_second == z_real + vz_real * time_i_second)
     if part == 2:
         solver.check()
         model = solver.model()
